81d4fb064bb6e78cf315a542f283ae0f8d9362dd
Thieu Mao <nguyen.van.thieub@framgia.com>
2017-01-29 18:03:57 +0700

    Add Example

---
+++ app/src/main/java/com/haynhanh/gmailapi/ApiAsyncTask.java
@@ -0,0 +1,127 @@
+package com.haynhanh.gmailapi;
+
+/**
+ * Created by thieumao on 1/29/17.
+ */
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiAsyncTask extends AsyncTask<Void, Void, Void> {
+    private TestActivity mActivity;
+    private GoogleAccountCredential credential;
+
+    private static final String USER_ID = "me";
+    private static final String TO = "test.caramelo@gmail.com";
+    private static final String FROM = "test.caramelo@gmail.com";
+
+    ApiAsyncTask(TestActivity activity, GoogleAccountCredential credential) {
+        this.mActivity = activity;
+        this.credential = credential;
+    }
+
+    /**
+     * Background task to call Gmail API.
+     * @param params no parameters needed for this task.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        /*
+        try {
+            String token = credential.getToken();
+            Log.d("CredentialTask", "token:\n" + token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GoogleAuthException e) {
+            e.printStackTrace();
+        }
+        */
+
+        try {
+            mActivity.clearResultsText();
+            mActivity.updateResultsText(getDataFromApi());
+
+            List<Message> messages = listAllMessages(USER_ID);
+            if(messages != null && messages.size() > 0){
+                GmailUtil.getMessage(mActivity.mService, USER_ID, messages.get(0).getId(), "raw");
+            }
+
+            GmailUtil.sendMessage(mActivity.mService, USER_ID, GmailUtil.createEmail(TO, FROM, "Subject", "Body"));
+
+        } catch (final GooglePlayServicesAvailabilityIOException availabilityException) {
+            mActivity.showGooglePlayServicesAvailabilityErrorDialog(
+                    availabilityException.getConnectionStatusCode());
+
+        } catch (UserRecoverableAuthIOException userRecoverableException) {
+            mActivity.startActivityForResult(
+                    userRecoverableException.getIntent(),
+                    TestActivity.REQUEST_AUTHORIZATION);
+
+        } catch (Exception e) {
+            mActivity.updateStatus("The following error occurred:\n" +
+                    e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Fetch a list of Gmail labels attached to the specified account.
+     * @return List of Strings labels.
+     * @throws IOException
+     */
+    private List<String> getDataFromApi() throws IOException {
+        // Get the labels in the user's account.
+        String user = "me";
+        List<String> labels = new ArrayList<String>();
+        ListLabelsResponse listResponse =
+                mActivity.mService.users().labels().list(user).execute();
+        for (Label label : listResponse.getLabels()) {
+            labels.add(label.getName());
+        }
+        return labels;
+    }
+
+    /**
+     * List all Messages of the user's mailbox with labelIds applied.
+     *
+     * @param userId User's email address. The special value "me"
+     * can be used to indicate the authenticated user.
+     * @throws IOException
+     */
+    public List<Message> listAllMessages(String userId) throws IOException {
+        System.out.println("listMessagesWithLabels");
+
+        ListMessagesResponse response = mActivity.mService.users().messages().list(userId).execute();
+
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = mActivity.mService.users().messages().list(userId)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+
+        for (Message message : messages) {
+            System.out.println(message.toPrettyString());
+        }
+
+        return messages;
+    }
+}
---
+++ app/src/main/java/com/haynhanh/gmailapi/GmailUtil.java
@@ -0,0 +1,173 @@
+package com.haynhanh.gmailapi;
+
+/**
+ * Created by thieumao on 1/29/17.
+ */
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GmailUtil {
+
+    public static MimeMessage createEmail(String to, String from, String subject, String bodyText) throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
+    }
+
+    public static Message createMessageWithEmail(MimeMessage emailContent) throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws MessagingException, IOException {
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+        return message;
+    }
+
+    public static Message getMessage(Gmail service, String userId, String messageId, String format) throws IOException {
+        Message message = null;
+        if(format != null && !format.isEmpty()) {
+            message = service.users().messages().get(userId, messageId).setFormat(format).execute();
+        } else {
+            message = service.users().messages().get(userId, messageId).execute();
+        }
+
+        System.out.println("Message snippet: " + message.getSnippet());
+        System.out.println("-----------------------------------------------****");
+        System.out.println("Message raw: " + Util.base64UrlDecode(message.getRaw()));
+        System.out.println("Message raw Base64 android: " + Base64.encodeBase64URLSafeString(message.decodeRaw()));
+        System.out.println("-----------------------------------------------****");
+
+        return message;
+    }
+
+    public static List<Message> listMessagesMatchingQuery(Gmail service, String userId, String query) throws IOException {
+        System.out.println("listMessagesMatchingQuery");
+        ListMessagesResponse response = service.users().messages().list(userId).setQ(query).execute();
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(userId).setQ(query)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+
+        for (Message message : messages) {
+            System.out.println(message.toPrettyString());
+        }
+
+        return messages;
+    }
+
+    public static List<Message> listMessagesWithLabels(Gmail service, String userId, List<String> labelIds) throws IOException {
+        System.out.println("listMessagesWithLabels");
+        ListMessagesResponse resp = service.users().messages().list(userId).execute();
+        System.out.println("messages");
+        System.out.println(resp.getMessages());
+
+
+        ListMessagesResponse response = service.users().messages().list(userId)
+                .setLabelIds(labelIds).execute();
+
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(userId).setLabelIds(labelIds)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+
+        for (Message message : messages) {
+            System.out.println(message.toPrettyString());
+        }
+
+        return messages;
+    }
+
+    public static List<Message> listAllMessages(Gmail service, String userId) throws IOException {
+        System.out.println("listMessagesWithLabels");
+        ListMessagesResponse response = service.users().messages().list(userId).execute();
+        System.out.println("messages");
+        System.out.println(response.getMessages());
+
+
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(userId)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+
+        for (Message message : messages) {
+            System.out.println(message.toPrettyString());
+        }
+
+        return messages;
+    }
+
+
+    public static List<String> getLabels(Gmail service, String userId){
+        ListLabelsResponse listResponse = null;
+        List<String> labelsStr = new ArrayList();
+
+        try {
+            listResponse = service.users().labels().list(userId).execute();List<Label> labels = listResponse.getLabels();
+
+            if (labels.size() == 0) {
+                System.out.println("No labels found.");
+            } else {
+                System.out.println("Labels:");
+                for (Label label : labels) {
+                    System.out.printf("- %s\n", label.getName());
+                    labelsStr.add(label.getId());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return labelsStr;
+    }
+
+}
